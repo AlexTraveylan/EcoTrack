3e677a849ed340ee58dc22273c90bf2273c750cd
Timothee DEMARES <timothee.demares@francetravail.fr>
2024-12-19 14:14:55 +0100

    feature: ajout de la bonne pratique d'optimisation des images

--- lib/best-practice.service.ts
+++ lib/best-practice.service.ts
@@ -1,46 +1,133 @@
-import type { Result } from "lighthouse"
+import type { Result } from "lighthouse";
 
 interface BestPractice {
-    title: string
-    checkIfValid(): boolean
-    getAcceptanceMessage(): string
-    displayMessages(): string[]
+  title: string;
+  refCode: string;
+  checkIfValid(): boolean;
+  getAcceptanceMessage(): string;
+  displayMessages(): string[];
 }
 
 export class BPLazyLoading implements BestPractice {
-    public readonly title: string = "Mettre en place le Lazy-loading"
-    private invalidUrls: string[] | null = null
-    private readonly acceptanceValue = 0
+  public readonly title: string = "Mettre en place le Lazy-loading";
+  public readonly refCode: string = "UI-01";
+  private invalidUrls: string[] | null = null;
+  private readonly acceptanceValue = 0;
 
   constructor(private readonly lhr: Result) {
-    this.lhr = lhr
+    this.lhr = lhr;
   }
-    checkIfValid(): boolean {
-        return this.getInvalidImageUrls().length === this.acceptanceValue
-    }
-    getAcceptanceMessage(): string {
-        return `Les images sous la ligne de flotaison sont en lazy loading (Requis: ${this.acceptanceValue}, Valeur: ${this.getInvalidImageUrls().length})`
-    }
-    displayMessages(): string[] {
-        return this.getInvalidImageUrls()
+  checkIfValid(): boolean {
+    return this.getInvalidImageUrls().length === this.acceptanceValue;
+  }
+  getAcceptanceMessage(): string {
+    return `Les images sous la ligne de flotaison sont en lazy loading (Requis: ${
+      this.acceptanceValue
+    }, Valeur: ${this.getInvalidImageUrls().length})`;
+  }
+  displayMessages(): string[] {
+    return this.getInvalidImageUrls();
+  }
+
+  private getInvalidImageUrls(): string[] {
+    if (this.invalidUrls !== null) {
+      return this.invalidUrls;
     }
 
-    private getInvalidImageUrls(): string[] {
+    const lazyAudit = this.lhr.audits["offscreen-images"];
+    // @ts-ignore I dont know how to get the type from lighthouse here.
+    const items = lazyAudit.details.items;
 
-        if (this.invalidUrls !== null) {
-            return this.invalidUrls
-        }
-
-        const lazyAudit = this.lhr.audits["offscreen-images"];
-        // @ts-ignore I dont know how to get the type from lighthouse here.
-        const items = lazyAudit.details.items;
-        
-        if (!Array.isArray(items)) {
-            return [];
-        }
-
-        this.invalidUrls = items.map(item => item.url).filter(url => typeof url === 'string');
-        
-        return this.invalidUrls
+    if (!Array.isArray(items)) {
+      return [];
     }
+
+    this.invalidUrls = items
+      .map((item) => item.url)
+      .filter((url) => typeof url === "string");
+
+    return this.invalidUrls;
+  }
+}
+
+export class BPOptimizeImages implements BestPractice {
+  public readonly title: string =
+    "Utiliser les images de manière approprié (taille, compression)";
+  public readonly refCode: string = "UI-20 / UI-21 / UI-22";
+  private unsizedImgUrls: string[] | null = null;
+  private unoptimizedImgUrls: string[] | null = null;
+  private readonly acceptanceValue = 0;
+
+  constructor(private readonly lhr: Result) {
+    this.lhr = lhr;
+  }
+
+  checkIfValid(): boolean {
+    return this.totalInvalidImgs() === this.acceptanceValue;
+  }
+  getAcceptanceMessage(): string {
+    return `Toutes les images sont optimisées (Requis: ${
+      this.acceptanceValue
+    }, Valeur: ${this.totalInvalidImgs()})`;
+  }
+
+  displayMessages(): string[] {
+    const messages: string[] = [];
+
+    this.getUnsizedImgUrls().forEach((url) => {
+      messages.push(`Dimensionnement incorrect: ${url}`);
+    });
+
+    this.getUnoptimizedImgUrls().forEach((url) => {
+      messages.push(`Compression incorrecte: ${url}`);
+    });
+
+    return messages;
+  }
+
+  private totalInvalidImgs(): number {
+    return (
+      this.getUnsizedImgUrls().length + this.getUnoptimizedImgUrls().length
+    );
+  }
+
+  private getUnsizedImgUrls(): string[] {
+    if (this.unsizedImgUrls !== null) {
+      return this.unsizedImgUrls;
+    }
+
+    const lazyAudit = this.lhr.audits["unsized-images"];
+    // @ts-ignore I dont know how to get the type from lighthouse here.
+    const items = lazyAudit.details.items;
+
+    if (!Array.isArray(items)) {
+      return [];
+    }
+
+    this.unsizedImgUrls = items
+      .map((item) => item.url)
+      .filter((url) => typeof url === "string");
+
+    return this.unsizedImgUrls;
+  }
+
+  private getUnoptimizedImgUrls(): string[] {
+    if (this.unoptimizedImgUrls !== null) {
+      return this.unoptimizedImgUrls;
+    }
+
+    const lazyAudit = this.lhr.audits["uses-optimized-images"];
+    // @ts-ignore I dont know how to get the type from lighthouse here.
+    const items = lazyAudit.details.items;
+
+    if (!Array.isArray(items)) {
+      return [];
+    }
+
+    this.unoptimizedImgUrls = items
+      .map((item) => item.url)
+      .filter((url) => typeof url === "string");
+
+    return this.unoptimizedImgUrls;
+  }
 }
--- lib/best-practice.test.ts
+++ lib/best-practice.test.ts
@@ -1,14 +1,22 @@
 import type { Result } from "lighthouse";
-import { BPLazyLoading } from "./best-practice.service";
-import { readFileSync } from 'fs';  // Importer le module fs
+import { BPLazyLoading, BPOptimizeImages } from "./best-practice.service";
+import { readFileSync } from "fs"; // Importer le module fs
 import { settings } from "./settings";
-import path from "path"
+import path from "path";
+
+// Bonne pratique du Lazy Loading
 
 describe("BPLazyLoading-TMF", () => {
   let result: Result;
 
   beforeEach(() => {
-    const jsonData = readFileSync(path.join(settings.workspace, "public/chercher-ma-formation/soudeur/1.json"), "utf-8");
+    const jsonData = readFileSync(
+      path.join(
+        settings.workspace,
+        "public/chercher-ma-formation/soudeur/1.json"
+      ),
+      "utf-8"
+    );
     result = JSON.parse(jsonData) as Result;
   });
 
@@ -39,33 +47,148 @@ describe("BPLazyLoading-TMF", () => {
 });
 
 describe("BPLazyLoading-HP", () => {
-    let result: Result;
-  
-    beforeEach(() => {
-      const jsonData = readFileSync(path.join(settings.workspace, "public/home-page/accueil/1.json"), "utf-8");
-      result = JSON.parse(jsonData) as Result;
-    });
-  
-    it("should have the correct title", () => {
-      const bpLazyLoading = new BPLazyLoading(result);
-      expect(bpLazyLoading.title).toBe("Mettre en place le Lazy-loading");
-    });
-  
-    it("should return true if there are no invalid image URLs", () => {
-      const bpLazyLoading = new BPLazyLoading(result);
-      expect(bpLazyLoading.checkIfValid()).toBe(true);
-    });
-  
-    it("should return an acceptance message with correct values", () => {
-      const bpLazyLoading = new BPLazyLoading(result);
-      const message = bpLazyLoading.getAcceptanceMessage();
-      expect(message).toContain("Requis: 0");
-      expect(message).toContain("Valeur: 0");
-    });
-  
-    it("should return empty invalid image URLs", () => {
-      const bpLazyLoading = new BPLazyLoading(result);
-      const messages = bpLazyLoading.displayMessages();
-      expect(messages).toEqual([]);
-    });
+  let result: Result;
+
+  beforeEach(() => {
+    const jsonData = readFileSync(
+      path.join(settings.workspace, "public/home-page/accueil/1.json"),
+      "utf-8"
+    );
+    result = JSON.parse(jsonData) as Result;
   });
+
+  it("should have the correct title", () => {
+    const bpLazyLoading = new BPLazyLoading(result);
+    expect(bpLazyLoading.title).toBe("Mettre en place le Lazy-loading");
+  });
+
+  it("should return true if there are no invalid image URLs", () => {
+    const bpLazyLoading = new BPLazyLoading(result);
+    expect(bpLazyLoading.checkIfValid()).toBe(true);
+  });
+
+  it("should return an acceptance message with correct values", () => {
+    const bpLazyLoading = new BPLazyLoading(result);
+    const message = bpLazyLoading.getAcceptanceMessage();
+    expect(message).toContain("Requis: 0");
+    expect(message).toContain("Valeur: 0");
+  });
+
+  it("should return empty invalid image URLs", () => {
+    const bpLazyLoading = new BPLazyLoading(result);
+    const messages = bpLazyLoading.displayMessages();
+    expect(messages).toEqual([]);
+  });
+});
+
+// Bonne Pratique d'optimisation des images
+
+describe("BPOptimizeImages-TMF", () => {
+  let result: Result;
+
+  beforeEach(() => {
+    const jsonData = readFileSync(
+      path.join(
+        settings.workspace,
+        "public/chercher-ma-formation/soudeur/1.json"
+      ),
+      "utf-8"
+    );
+    result = JSON.parse(jsonData) as Result;
+  });
+
+  it("should have the correct title", () => {
+    const bpOptimizeImages = new BPOptimizeImages(result);
+    expect(bpOptimizeImages.title).toBe(
+      "Utiliser les images de manière approprié (taille, compression)"
+    );
+  });
+
+  it("should return false if there are invalid image URLs", () => {
+    const bpOptimizeImages = new BPOptimizeImages(result);
+    expect(bpOptimizeImages.checkIfValid()).toBe(false);
+  });
+
+  it("should return an acceptance message with correct values", () => {
+    const bpOptimizeImages = new BPOptimizeImages(result);
+    const message = bpOptimizeImages.getAcceptanceMessage();
+    expect(message).toContain("Requis: 0");
+    expect(message).toContain("Valeur: 3");
+  });
+
+  it("should return invalid image URLs", () => {
+    const bpOptimizeImages = new BPOptimizeImages(result);
+    const messages = bpOptimizeImages.displayMessages();
+    expect(messages).toEqual([
+      "Dimensionnement incorrect: https://www.francetravail.fr/files/live/sites/PE/files/images/Logos/header-logo2023-ft-fr.svg",
+      "Dimensionnement incorrect: https://www.francetravail.fr/files/live/sites/PE/files/images/Logos/header-logo2021-marianne.svg",
+      "Dimensionnement incorrect: https://voxusagers.numerique.gouv.fr/static/bouton-blanc.svg",
+    ]);
+  });
+});
+
+describe("BPOptimizeImages-HP", () => {
+  let result: Result;
+
+  beforeEach(() => {
+    const jsonData = readFileSync(
+      path.join(settings.workspace, "public/home-page/accueil/1.json"),
+      "utf-8"
+    );
+    result = JSON.parse(jsonData) as Result;
+  });
+
+  it("should have the correct title", () => {
+    const bpOptimizeImages = new BPOptimizeImages(result);
+    expect(bpOptimizeImages.title).toBe(
+      "Utiliser les images de manière approprié (taille, compression)"
+    );
+  });
+
+  it("should return false if there are invalid image URLs", () => {
+    const bpOptimizeImages = new BPOptimizeImages(result);
+    expect(bpOptimizeImages.checkIfValid()).toBe(false);
+  });
+
+  it("should return an acceptance message with correct values", () => {
+    const bpOptimizeImages = new BPOptimizeImages(result);
+    const message = bpOptimizeImages.getAcceptanceMessage();
+    expect(message).toContain("Requis: 0");
+    expect(message).toContain("Valeur: 28");
+  });
+
+  it("should return empty invalid image URLs", () => {
+    const bpOptimizeImages = new BPOptimizeImages(result);
+    const messages = bpOptimizeImages.displayMessages();
+    expect(messages).toEqual([
+      "Dimensionnement incorrect: https://www.francetravail.fr/files/live/sites/PE/files/masters/hp-v4/home-cover-1-bis.jpg",
+      "Dimensionnement incorrect: https://www.francetravail.fr/files/live/sites/PE/files/dri/FAQ-prime-exceptionnelle.gif",
+      "Dimensionnement incorrect: https://www.francetravail.fr/files/live/sites/PE/files/affiche/2024/prendre-soin--fr-308.jpg",
+      "Dimensionnement incorrect: https://www.francetravail.fr/files/live/sites/PE/files/pole-emploi-internet/actualite/grande-cause-inclusion-make-org-308.jpg",
+      "Dimensionnement incorrect: https://www.francetravail.fr/files/live/sites/PE/files/employeur/2024/logo-FT-Pro-308.gif",
+      "Dimensionnement incorrect: https://www.francetravail.fr/files/live/sites/PE/files/pole-emploi-internet/actualite/G-P/la-poste-recrute-308.jpg",
+      "Dimensionnement incorrect: https://www.francetravail.fr/files/live/sites/PE/files/secteurs-metiers/Fonction-publique/fonction-publique-statut-contractuel-308.jpg",
+      "Dimensionnement incorrect: https://www.francetravail.fr/files/live/sites/PE/files/secteurs-metiers/Artisanat/charcutier-traiteur-308.jpg",
+      "Dimensionnement incorrect: https://www.francetravail.fr/files/live/sites/PE/files/secteurs-metiers/Industrie/pharmaceutique/pros-des-labos-308.jpg",
+      "Dimensionnement incorrect: https://www.francetravail.fr/files/live/sites/PE/files/affiche/Logos/Bloc_Marque_RF_France_Travail_marque-308.jpg",
+      "Dimensionnement incorrect: https://www.francetravail.fr/files/live/sites/PE/files/images/Logos/header-logo2023-ft-fr.svg",
+      "Dimensionnement incorrect: https://www.francetravail.fr/files/live/sites/PE/files/bannieres/2024/banni%c3%a8re-FranceTravail-Pro.gif",
+      "Dimensionnement incorrect: https://www.francetravail.fr/files/live/sites/PE/files/bannieres/2024/banni%c3%a8re-FranceTravail-Pro.gif",
+      "Dimensionnement incorrect: https://www.francetravail.fr/files/live/sites/PE/files/bannieres/2024/banni%c3%a8re-FranceTravail-Pro.gif",
+      "Dimensionnement incorrect: https://www.francetravail.fr/files/live/sites/PE/files/bannieres/2024/ban-prendre-soin.gif",
+      "Dimensionnement incorrect: https://www.francetravail.fr/files/live/sites/PE/files/bannieres/2024/france-services-banniere.gif",
+      "Dimensionnement incorrect: https://www.francetravail.fr/files/live/sites/PE/files/images/Logos/header-logo2021-marianne.svg",
+      "Dimensionnement incorrect: https://www.francetravail.fr/files/live/sites/PE/files/nouveau%20site/candidat/mode-emploi-NL-308.png",
+      "Dimensionnement incorrect: https://www.francetravail.fr/files/live/sites/PE/files/nouveau%20site/candidat/service-en-ligne/simulateur-2-308-transparent.gif",
+      "Dimensionnement incorrect: https://www.francetravail.fr/files/live/sites/PE/files/nouveau%20site/candidat/service-en-ligne/services-publics-3086HP.png",
+      "Compression incorrecte: https://www.francetravail.fr/files/live/sites/PE/files/secteurs-metiers/Fonction-publique/fonction-publique-statut-contractuel-308.jpg",
+      "Compression incorrecte: https://www.francetravail.fr/files/live/sites/PE/files/secteurs-metiers/Industrie/pharmaceutique/pros-des-labos-308.jpg",
+      "Compression incorrecte: https://www.francetravail.fr/files/live/sites/PE/files/pole-emploi-internet/actualite/grande-cause-inclusion-make-org-308.jpg",
+      "Compression incorrecte: https://www.francetravail.fr/files/live/sites/PE/files/secteurs-metiers/Artisanat/charcutier-traiteur-308.jpg",
+      "Compression incorrecte: https://www.francetravail.fr/files/live/sites/PE/files/masters/hp-v4/home-cover-1-bis.jpg",
+      "Compression incorrecte: https://www.francetravail.fr/files/live/sites/PE/files/pole-emploi-internet/actualite/G-P/la-poste-recrute-308.jpg",
+      "Compression incorrecte: https://www.francetravail.fr/files/live/sites/PE/files/affiche/Logos/Bloc_Marque_RF_France_Travail_marque-308.jpg",
+      "Compression incorrecte: https://www.francetravail.fr/files/live/sites/PE/files/affiche/2024/prendre-soin--fr-308.jpg",
+    ]);
+  });
+});
