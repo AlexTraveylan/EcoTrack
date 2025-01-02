6433603c53c04095e77123b2719a06bec0368c75
AlexTraveylan <monsieurdemares@gmail.com>
2025-01-02 21:15:01 +0100

    fix: region needed for github CI

--- lib/settings.ts
+++ lib/settings.ts
@@ -2,7 +2,7 @@ import path from "path"
 
 export const settings = {
   workspace: path.resolve(__dirname, "../"),
-  s3Region: process.env.S3_REGION || "",
+  s3Region: process.env.S3_REGION || "us-east-1",
   s3Endpoint: process.env.S3_ENDPOINT || "",
   s3AccessKeyId: process.env.S3_ACCES_KEY_ID || "",
   s3SecretAccessKey: process.env.S3_SECRET_ACCESS_KEY || "",
